86bda072beeb6e68022673d9225aff765c371bcb
Gustavo Martins <gustavomartins.n@gmail.com>
2024-08-17 13:06:21 -0300

    Adicionando comentários ao código

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Clique do botão com o id 'cta-button'
 document.getElementById('cta-button').addEventListener('click', function() {
-    alert('“Botão clicado”');
+    alert('“Botão clicado”'); // Alerta quando o botão é clicado
 });
 
+// Envio ao formulário
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
+    event.preventDefault(); // Prevenção ao envio padrão do formulário
+    alert('Formulário enviado com sucesso!'); // Alerta quando o formulário é enviado
 });
