0fd2ba30f1cf23437e7c67fe604ab47782ac0964
Gustavo Martins <gustavomartins.n@gmail.com>
2024-08-18 15:56:18 -0300

    Última atualização

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Clique do botão com o id 'cta-button'
+// botão.
 document.getElementById('cta-button').addEventListener('click', function() {
-    alert('“Botão clicado”'); // Alerta quando o botão é clicado
+    alert('Botão clicado');
 });
 
-// Envio ao formulário
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenção ao envio padrão do formulário
-    alert('Formulário enviado com sucesso!'); // Alerta quando o formulário é enviado
+// formulário id.
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o envio padrão do formulário
+    alert('Formulário enviado com sucesso!');
 });
